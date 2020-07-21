ddadc77740d91acaa6959c6b8e9abfef64816133
chiaki <chi3710@gmail.com>
2020-07-21 13:13:44 +0900

    [Add]controller/routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,37 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, controllers: {
+  	sessions: 'admin/sessions',
+    passwords: 'admin/passwords',
+    registrations: 'admin/registrations'
+  }
+
+  namespace :admin do
+  	get 'top' => 'home#top'
+  	resources :users, only: [:index, :show, :edit, :update]
+  	resources :posts
+  	resources :genres, only: [:index, :create, :edit, :update]
+  	resources :prefectures, only: [:index]
+  	resources :areas, only: [:index, :create, :edit, :update]
+  end
+
+  devise_for :users, controllers: {
+  	sessions: 'public/sessions',
+    passwords: 'public/passwords',
+    registrations: 'public/registrations'
+  }
+
+  scope module: :public do
+  	root 'home#top'
+  	get 'about' => 'home#about'
+  	get 'confirm' => 'users#confirm'
+  	put 'confirm' => 'users#withdraw'
+  	get 'mypage' => 'users#show'
+  	get 'mypage/edit' => 'users#edit'
+  	patch 'mypage' => 'users#update'
+  	resources :posts
+  	resources :genres, only: [:index]
+  	resources :prefectures, only: [:index]
+  	resources :areas, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
