4be5f487cd194b4da3856c2bcbc595487339f6f3
chiaki <chi3710@gmail.com>
2020-08-04 11:48:13 +0900

    [modified]layout/[add]fontawesome

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,13 +2,13 @@ class Public::HomesController < ApplicationController
 
 	def top
 		@areas = Area.all
-		@genres = Genre.all
+		@genres = Genre.limit(8)
 		# 投稿をもつタグのみ表示
 		@tags = Tag.joins(:posts).where("posts.id is NOT NULL")
 		#新着投稿
-		@new_posts = Post.limit(3).order("created_at DESC")
+		@new_posts = Post.limit(4).order("created_at DESC")
 		#いいね数が多い投稿
-		@liked_posts = Post.find(Favorite.group(:post_id).order('count(post_id)desc').limit(3).pluck(:post_id))
+		@liked_posts = Post.find(Favorite.group(:post_id).order('count(post_id)desc').limit(4).pluck(:post_id))
 	end
 
 	def about
