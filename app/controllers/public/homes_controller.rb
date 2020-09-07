1511675d93240d244181d81d69e0e8f0a3a9d9c0
chiaki <chi3710@gmail.com>
2020-09-07 13:07:55 +0900

    [modified]responsive_layout

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -13,7 +13,7 @@ class Public::HomesController < ApplicationController
 			@histories = current_user.browsing_histories.includes(:post, post: [:user, :prefecture, :genre]).limit(4).order("created_at DESC")
 			# タイムライン
 		    @users = current_user.following_user
-		    @following_posts = Post.includes(:user, :genre, :prefecture).where(user_id: @users).limit(10).order("created_at DESC")
+		    @following_posts = Post.includes(:user, :genre, :prefecture).where(user_id: @users).limit(8).order("created_at DESC")
 		end
 	end
 end
