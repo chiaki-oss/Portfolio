63af6b0116bd736b06a3a3201cc4825d36d03659
chiaki <chi3710@gmail.com>
2020-09-08 18:46:37 +0900

    [add]https

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -9,7 +9,7 @@ class Public::HomesController < ApplicationController
 		if user_signed_in?
 			@new_posts = Post.includes(:user, :prefecture, :genre).limit(6)
 			# いいね数が多い投稿
-			@liked_posts = Post.includes(:user, :genre, :prefecture).except(:order).find(Favorite.group(:post_id).order('count(post_id)desc').limit(4).pluck(:post_id))
+			@liked_posts = Post.includes(:user, :genre, :prefecture).except(:order).find(Favorite.group(:post_id).order('count(post_id)desc').limit(6).pluck(:post_id))
 			# 閲覧履歴
 			@histories = current_user.browsing_histories.includes(:post, post: [:user, :prefecture, :genre]).limit(3).reverse_order
 			# タイムライン
