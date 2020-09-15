44c1c2ea87dc8559b8b21c9c36fc2c39fc2b73fd
chiaki <chi3710@gmail.com>
2020-09-15 16:54:45 +0900

    [modified]layout

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -3,6 +3,7 @@ class Admin::HomesController < ApplicationController
 	
 	def top
 		@new_contatcts = Contact.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).count
+		@need_reply = Contact.where(reply: nil).count
 		@new_users = User.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).count
 		@new_posts = Post.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).count
 	end
